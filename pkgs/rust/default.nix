00e9c6df105a804a3054ffcd919990bf7e0155a1
FitzHawke <will@fitzhawk.com>
2025-05-06 01:28:27 -0400

    rust-base: add rust analyzer

--- pkgs/rust/default.nix
+++ pkgs/rust/default.nix
@@ -3,6 +3,7 @@
     name = "rust-base";
     packages = with pkgs; [
       rust-bin.stable.latest.default
+      rust-analyzer
     ];
     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
     DIRENV_LOG_FORMAT = "";
